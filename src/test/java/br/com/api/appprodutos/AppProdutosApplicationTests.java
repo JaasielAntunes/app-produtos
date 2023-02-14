62bffc9465e6214fff909058480f06d3f41cb718
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2023-02-14 16:15:56 -0300

    Projeto criado

---
+++ src/test/java/br/com/api/appprodutos/AppProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.appprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppProdutosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
